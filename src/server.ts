30ea75f36a7b03784ae7b2feac47c1feb97732b1
Ion Popsoi <ionmanager@gmail.com>
2020-02-26 19:22:32 +0000

    feat: changed default port

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
 import 'dotenv/config';
 import app from './app';
 
-const PORT = process.env.PORT || 3333;
+const PORT = process.env.PORT || 5443;
 
 app.listen(PORT, () => {
     console.log('⚡️ Server listening on http://localhost:%d', PORT);
